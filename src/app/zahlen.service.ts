e2fb78bc1da1c51fa34781d5f67b8c9063d521ba
Youramon <bay000r.man@gmail.com>
2024-02-16 17:40:11 +0100

    KGV Rechner 🤣🤣😘😘👌

--- src/app/zahlen.service.ts
+++ src/app/zahlen.service.ts
@@ -7,5 +7,25 @@ export class ZahlenService {
 public random(min: number, max: number) { 
   return Math.round(Math.random() * (max - min) ) + min;
 }
+public kgvRechner(num1: number, num2: number): number {
+  const gcd = (a: number, b: number): number => {
+    return b === 0 ? a : gcd(b, a % b);
+  };
+
+  const lcm = (a: number, b: number): number => {
+    return (a * b) / gcd(a, b);
+  };
+
+  const a = lcm(num1, num2);
+  if (a/Math.min(num1,num2) <= 200) {
+    return a;
+  } else {
+    return 0;
+  }
+}
+
+
+
+
   constructor() { }
 }
