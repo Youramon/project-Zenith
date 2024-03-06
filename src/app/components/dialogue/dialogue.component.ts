19ef20b9b51180e8439974eac52b478415922fc7
Youramon <bay000r.man@gmail.com>
2024-03-06 18:15:43 +0100

    Yoooly shirt es funktionier

--- src/app/components/dialogue/dialogue.component.ts
+++ src/app/components/dialogue/dialogue.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import * as db from '../../meine-Dateien/db';
 import { ZahlenService } from '../../zahlen.service';
 import { FighterComponent } from '../fighter/fighter.component';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonToggleChange, MatButtonToggleModule} from '@angular/material/button-toggle';
 @Component({
   selector: 'app-dialogue',
   standalone: true,
@@ -14,6 +14,12 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 export class DialogueComponent {
   ObjektEins= this.tabs[this.zahlen.random(0, db.tabs.length - 1)];
   ObjektZwei= this.tabs[this.zahlen.random(0, db.tabs.length - 1)];
+ 
+handleFight(){
+  
+}
+
+
   constructor(public zahlen: ZahlenService){
 
   }
---
+++ src/app/meine-Dateien/handleKampf.ts
@@ -0,0 +1,9 @@
+export handleKampf(objektEins, objektZwei, ergebnis){
+ if(ergebnis = "win"){
+    
+ }
+}
+
+private calculateElo(): void {
+    
+}
