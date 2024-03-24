174f40b2f15f8cc9b14b534beb29010d3bac8514
Youramon <bay000r.man@gmail.com>
2024-03-24 23:25:28 +0100

    Dark mode? 🥺

--- src/app/meine-Dateien/handleKampf.ts
+++ src/app/meine-Dateien/handleKampf.ts
@@ -1,4 +1,4 @@
 export const handleKampf = (player1rating: number, player2rating: number, result: number): number => {
-    const expected1 = 1 / (1 + 10 ** ((player2rating - player1rating) / 400));
-    return Math.round(32 * (result - expected1));
+    const expected = 1 / (1 + 10 ** ((player2rating - player1rating) / 400));
+    return Math.round(32 * (result - expected));
   };
